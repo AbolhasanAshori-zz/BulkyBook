e43f69ec1b33e907261f326a77a60c055e95af20
Abolhasan Ashori <abolhasan.ashori@gmail.com>
2021-06-01 17:03:18 +0430

    Cover Type (Stored Procedure)

---
+++ BulkyBook.Models/CoverType.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class CoverType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Cover Type")]
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
