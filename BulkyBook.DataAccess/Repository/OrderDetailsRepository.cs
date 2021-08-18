d38411e0883cc38938ba5184a3f66685d48c9edd
Abolhasan Ashori <abolhasan.ashori@gmail.com>
2021-08-18 21:20:52 +0430

    Shopping Cart & Order Models and Repositories

---
+++ BulkyBook.DataAccess/Repository/OrderDetailsRepository.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class OrderDetailsRepository : Repository<OrderDetails>, IOrderDetailsRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public OrderDetailsRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderDetails obj)
+        {
+            _db.Update(obj);
+        }
+    }
+}
