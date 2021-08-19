3f8a3be799b8088101b240ca4b14d120837a3a31
Abolhasan Ashori <abolhasan.ashori@gmail.com>
2021-08-19 15:41:19 +0430

    Shopping Cart Management

---
+++ BulkyBook.Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCart> ListCart { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+
+    }
+}
