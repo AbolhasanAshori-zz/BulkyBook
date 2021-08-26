117f3e46d99f4518d002672059aa6a7bb386f0b5
Abolhasan Ashori <abolhasan.ashori@gmail.com>
2021-08-26 18:06:43 +0430

    Tag Helper - Pagination

---
+++ BulkyBook.Models/PagingInfo.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace BulkyBook.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItem { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public string UrlParam { get; set; }
+
+        public int TotalPage => (int)Math.Ceiling((decimal)TotalItem / ItemsPerPage);
+    }
+}
---
+++ BulkyBook.Models/ViewModels/CategoryVM.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class CategoryVM
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
